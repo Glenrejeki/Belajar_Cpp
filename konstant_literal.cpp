377be866751b50c7fbe97b4955e757faa8d4bf02
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-04-24 11:05:03 +0700

    update

--- konstant_literal.cpp
+++ konstant_literal.cpp
@@ -4,6 +4,7 @@ int main (){
   std :: cout << 200 << std :: endl;
   std :: cout << 3.14 << std :: endl;
   std :: cout << "Glen Rejeki Sitorus" << std :: endl;
+  std :: coout << "Glen\n pemula pbo" std :: endl;
   std :: cout << 'B' << std :: endl;
   std :: cout << true << std :: endl;
   std :: cout << false << std :: endl;
