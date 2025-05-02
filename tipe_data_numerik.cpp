66b86c3e0e3aa4934fcf5df2b3b7d93b34debcd5
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-05-02 23:19:10 +0700

    Tipe data numerik done

---
+++ tipe_data_numerik.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+
+int main (){
+
+  int angka = 11;
+  unsigned int angka_positif = 100;
+  short angka_kecil = -100;
+  long long int angka_besar = 1000000000;
+
+
+  std :: cout << angka << std :: endl;
+  std ::cout  << angka_positif << std :: endl ;
+  std :: cout << angka_kecil << std :: endl;
+  std :: cout << angka_besar << std :: endl;
+  return 0;
+
+}
