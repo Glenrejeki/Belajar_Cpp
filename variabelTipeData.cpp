e2574b81e7e946d7424e334865b710e7cebdef1a
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-05-02 23:10:08 +0700

    variabel done

--- variabelTipeData.cpp
+++ variabelTipeData.cpp
@@ -8,8 +8,21 @@ int main (){
   umur = 19;
   std :: cout << "Umur si Glen Tahun Lalu adalah " << umur << " tahun"<<  std :: endl;
 
+
+  unsigned int ketinggian_bangunan = 100;
+  std :: cout << "Ketinggian bagunan :"<< ketinggian_bangunan << std :: endl;
+
+  unsigned int kedalaman_laut = -200;
+  std :: cout << "Kedalaman laut :" << kedalaman_laut << std :: endl; // ini mengubah nilai negatif menjadi positif 
+
   float pi = 3.14;
   std :: cout << "Nilai pi adalah " << pi << std :: endl ;
+
+
+  const float gravitasi_normal = 9.8;
+  // float gravitasi_normal = 10.0;  ini tidak bisa diubah
+  std :: cout << "Gravitasi tempat normal : " << gravitasi_normal << std :: endl;
+
 
 
   double nilai_pi_tinggi = 3.14159265358979323846;
