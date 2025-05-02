8270c3333e061b90982d88e3efa2a5039734f2f5
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-05-02 23:53:12 +0700

    multi variabel done bosku

---
+++ multi_variabel.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main (){
+
+  int angka1, angka2 = 11 , angka3;
+  angka1 = 3;
+  angka3 = 100;
+  std :: cout << angka1 << std :: endl;
+  std :: cout << angka2 << std :: endl;
+  std :: cout << angka3 << std :: endl;
+
+
+  int umur = 20 ;
+  float tinggi = 169.8;
+  char insial = 'j';
+  std :: cout << "Umur: " << umur << " Tinggi: "<< tinggi << "Insial: "<< insial<< std :: endl;
+  return 0;
+}
