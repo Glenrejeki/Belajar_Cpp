43d25faacd0266118a067f8c9aaf38153b1fd8f9
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-05-02 23:02:59 +0700

    init

--- konstant_literal.cpp
+++ konstant_literal.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int main (){
-  std :: cout << 200 << std :: endl;
-  std :: cout << 3.14 << std :: endl;
-  std :: cout << "Glen Rejeki Sitorus" << std :: endl;
-  std :: coout << "Glen\n pemula pbo" std :: endl;
-  std :: cout << 'B' << std :: endl;
-  std :: cout << true << std :: endl;
-  std :: cout << false << std :: endl;
-  std :: cout << nullptr << std :: endl;
-  return 0 ;
+int main() {
+    std::cout << 200 << std::endl;
+    std::cout << 3.14 << std::endl;
+    std::cout << "Glen Rejeki Sitorus" << std::endl;
+    std::cout << "Glen\n pemula pbo" << std::endl;
+    std::cout << 'B' << std::endl;
+    std::cout << true << std::endl;
+    std::cout << false << std::endl;
+    std::cout << "nullptr" << std::endl; // Changed to string literal
+    return 0;
 }
---
+++ variabelTipeData.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+int main (){
+  
+  int umur = 20;
+  std :: cout << umur << std :: endl;
+  umur = 19;
+  std :: cout << "Umur si Glen Tahun Lalu adalah " << umur << " tahun"<<  std :: endl;
+
+  float pi = 3.14;
+  std :: cout << "Nilai pi adalah " << pi << std :: endl ;
+
+
+  double nilai_pi_tinggi = 3.14159265358979323846;
+  std :: cout << nilai_pi_tinggi<< std :: endl ;
+
+  char karakter = 'C';
+  std :: cout << karakter << std :: endl ;
+
+  std ::string pesan = "Zeff ayok mabar ml"; // harus pakai std untuk pakai string
+  std::cout << pesan << std::endl;
+
+
+  bool logika = false ;
+  std :: cout << logika << std :: endl ;
+
+  return 0;
+
+}
